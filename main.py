8305aa4a5e76b84a88d6dfc0eee9c70b8116b080
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-03-27 10:49:08 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Definições
+
+def somar_dois_numeros(num1, num2):
+    return num1 + num2
+
+def subtrair_dois_numeros(num1, num2):
+    return num1 - num2
+
+def multiplicar_dois_numeros(num1, num2):
+    return num1 * num2
+
+def dividir_dois_numeros(num1, num2):
+    return num1 / num2
+
+# Requisições
+
+resultado = somar_dois_numeros(5, 7)
+print(f'A soma é {resultado}')
+
+resultado = subtrair_dois_numeros(7, 5)
+print(f' A subtração é {resultado}')
+
+resultado = multiplicar_dois_numeros(3, 5)
+print(f'A multiplicação é {resultado}')
+
+resultado = dividir_dois_numeros(8, 4)
+print(f'A divisão é {resultado}')
+
+
+# Teste
+
+def teste
